8aa7e858dc38fafa6b5993a33cd5b2d60fb2bdb2
Calvin Colton <calvincolton@Calvins-MacBook-Pro-2.local>
2023-12-21 14:34:32 -0600

    API and tests

---
+++ pomodoro/inmemory_test.go
@@ -0,0 +1,15 @@
+package pomodoro_test
+
+import (
+	"testing"
+
+	"github.com/calvincolton/go-cli-pomodoro/pomodoro"
+	"github.com/calvincolton/go-cli-pomodoro/pomodoro/repository"
+)
+
+func getRepo(t *testing.T) (pomodoro.Repository, func()) {
+	t.Helper()
+
+	// would normally return a cleanup function, but this is not necessary for in memory data
+	return repository.NewInMemoryRepo(), func() {}
+}
