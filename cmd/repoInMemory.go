ea52f21d7d8438400c8e47477c0929baa05437a1
Calvin Colton <calvincolton@Calvins-MacBook-Pro-2.local>
2023-12-24 21:58:49 -0600

    add cobra, BUT ITS BROKEN

---
+++ cmd/repoInMemory.go
@@ -0,0 +1,10 @@
+package cmd
+
+import (
+	"github.com/calvincolton/go-cli-pomodoro/pomodoro"
+	"github.com/calvincolton/go-cli-pomodoro/pomodoro/repository"
+)
+
+func getRepo() (pomodoro.Repository, error) {
+	return repository.NewInMemoryRepo(), nil
+}
